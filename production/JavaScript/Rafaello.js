1c5f3bbc61b6bd21b23b6210239aa4fdc32873a3
Alwyn Bester <alwyn@zoclee.com>
2019-01-22 21:01:05 +0200

    Added Rectangle.

--- production/JavaScript/Rafaello.js
+++ production/JavaScript/Rafaello.js
@@ -3,10 +3,50 @@
 
     Rafaello.BarChart = function (width, height, data) {
         var chart = '';
+        var style = JSON.parse('{}');
 
-        chart = '<svg width=\"100\" height=\"100\" xmlns=\"http://www.w3.org/2000/svg\"><circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"red\" stroke-width=\"4\" fill=\"yellow\" /></svg>';
+        chart = '<svg ';
+        chart = (((chart + " width=\"") + width) + "\"");
+        chart = (((chart + " height=\"") + height) + "\"");
+        chart = (chart + " xmlns=\"http://www.w3.org/2000/svg\">");
+
+        style['fill'] = '#cc0000';
+        style['stroke'] = '#00cc00';
+        style['stroke-width'] = '10';
+        chart = (chart + Rafaello.Rectangle(10, 10, 80, 80, style));
+
+        chart = (chart + "</svg>");
 
         return chart;
+    }
+
+    // Format style object as Xml attribute string
+    Rafaello.FormatStyle = function (style) {
+        var attrStr = '';
+        var i = 0;
+
+        i = 0;
+        while ((i < Object.keys(style).length)) {
+            attrStr = (((((attrStr + " ") + Object.keys(style)[i]) + "=\"") + style[Object.keys(style)[i]]) + "\"");
+            i = (i + 1);
+        }
+
+        return attrStr;
+    }
+
+    // Create a rect element
+    Rafaello.Rectangle = function (x, y, width, height, style) {
+        var element = '';
+
+        element = '<rect ';
+        element = (((element + " x=\"") + (x).toString()) + "\"");
+        element = (((element + " y=\"") + (y).toString()) + "\"");
+        element = (((element + " width=\"") + (width).toString()) + "\"");
+        element = (((element + " height=\"") + (height).toString()) + "\"");
+        element = (element + Rafaello.FormatStyle(style));
+        element = (element + " />");
+
+        return element;
     }
 
     window.Rafaello = Rafaello;
--- production/PHP/Rafaello.php
+++ production/PHP/Rafaello.php
@@ -5,10 +5,45 @@ class Rafaello {
 
     public static function BarChart($width, $height, $data) {
         $chart = "";
+        $style = json_decode("{}");
 
-        $chart = "<svg width=\"100\" height=\"100\" xmlns=\"http://www.w3.org/2000/svg\"><circle cx=\"50\" cy=\"50\" r=\"40\" stroke=\"red\" stroke-width=\"4\" fill=\"yellow\" /></svg>";
+        $chart = "<svg ";
+        $chart = ((($chart . " width=\"") . $width) . "\"");
+        $chart = ((($chart . " height=\"") . $height) . "\"");
+        $chart = ($chart . " xmlns=\"http://www.w3.org/2000/svg\">");
+
+        $style->{"fill"} = "#cc0000";
+        $style->{"stroke"} = "#00cc00";
+        $style->{"stroke-width"} = "10";
+        $chart = ($chart . self::Rectangle(10, 10, 80, 80, $style));
+
+        $chart = ($chart . "</svg>");
 
         return $chart;
+    }
+
+    // Format style object as Xml attribute string
+    public static function FormatStyle($style) {
+        $attrStr = "";
+
+        if (property_exists($style, "fill")) {
+            $attrStr = ((($attrStr . " fill=\"") . $style->{"fill"}) . "\"");
+        }
+
+        return $attrStr;
+    }
+
+    // Create a rect element
+    public static function Rectangle($x, $y, $width, $height, $style) {
+        $element = "";
+        $element = "<rect ";
+        $element = ((($element . " x=\"") . (string)($x)) . "\"");
+        $element = ((($element . " y=\"") . (string)($y)) . "\"");
+        $element = ((($element . " width=\"") . (string)($width)) . "\"");
+        $element = ((($element . " height=\"") . (string)($height)) . "\"");
+        $element = ($element . self::FormatStyle($style));
+        $element = ($element . " />");
+        return $element;
     }
 
 }
