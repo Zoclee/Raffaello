c5e5f4c1663fc6a1d135eceebecedbf7d03cb9c0
Alwyn Bester <alwyn.bester@futurelearn.co.za>
2019-01-23 13:42:21 +0200

    Partially implemented bar chart

--- production/JavaScript/Rafaello.js
+++ production/JavaScript/Rafaello.js
@@ -1,32 +1,61 @@
 (function () {
     var Rafaello = {};
 
-    // Generate BarChart from data
-    Rafaello.BarChart = function (width, height, data) {
+    // ***** BarChart ********************
+
+    Rafaello.BarChart = function (width, height, chartData) {
         var chart = '';
         var attr = JSON.parse('{}');
+        var points = 0;
+        var i = 0;
+        var max = 0;
 
         chart = '<svg ';
         chart = (((chart + " width=\"") + width) + "\"");
         chart = (((chart + " height=\"") + height) + "\"");
         chart = (chart + " xmlns=\"http://www.w3.org/2000/svg\">");
 
-        attr['x'] = 10;
-        attr['y'] = 10;
-        attr['width'] = 80;
-        attr['height'] = 80;
+        points = Object.keys(chartData['datasets'][0]['data']).length;
+
+        // configure styling attributes
+
+
+        attr['width'] = parseInt(Math.floor((width - ((points - 1) * 3)) / points));
         attr['fill'] = '#cc0000';
         attr['stroke'] = '#00cc00';
-        attr['stroke-width'] = 10;
-        chart = (chart + Rafaello.BuildElement('rect', attr));
+        attr['stroke-width'] = 1;
+
+        // determine maximum height
+
+
+        max = chartData['datasets'][0]['data'][0];
+        i = 1;
+        while ((i < points)) {
+            if ((chartData['datasets'][0]['data'][i] > max)) {
+                max = chartData['datasets'][0]['data'][i];
+            }
+            i = (i + 1);
+        }
+
+        // create bars
+
+        i = 0;
+        while ((i < points)) {
+            attr['x'] = ((attr['width'] * i) + (3 * i));
+            attr['height'] = (Math.round((height * (chartData['datasets'][0]['data'][i] / max)) * Math.pow(10, 0)) / Math.pow(10, 0));
+            attr['y'] = (height - attr['height']);
+            chart = (chart + Rafaello.rafBuildElement('rect', attr));
+            i = (i + 1);
+        }
 
         chart = (chart + "</svg>");
 
         return chart;
     }
 
-    // Build SVG element
-    Rafaello.BuildElement = function (name, attributes) {
+    // ***** rafBuildElement ********************
+
+    Rafaello.rafBuildElement = function (name, attributes) {
         var element = '';
         var i = 0;
 
