1337550e350d856eb401135b4a2a0d3353dc1b4c
Alwyn Bester <alwyn.bester@futurelearn.co.za>
2019-01-23 13:43:16 +0200

    Produced PHP library.

--- production/PHP/Rafaello.php
+++ production/PHP/Rafaello.php
@@ -3,32 +3,61 @@ class Rafaello {
     public function __construct() {
     }
 
-    // Generate BarChart from data
-    public static function BarChart($width, $height, $data) {
+    // ***** BarChart ********************
+
+    public static function BarChart($width, $height, $chartData) {
         $chart = "";
         $attr = json_decode("{}");
+        $points = 0;
+        $i = 0;
+        $max = 0;
 
         $chart = "<svg ";
         $chart = ((($chart . " width=\"") . $width) . "\"");
         $chart = ((($chart . " height=\"") . $height) . "\"");
         $chart = ($chart . " xmlns=\"http://www.w3.org/2000/svg\">");
 
-        $attr->{"x"} = 10;
-        $attr->{"y"} = 10;
-        $attr->{"width"} = 80;
-        $attr->{"height"} = 80;
+        $points = count($chartData->{"datasets"}[0]->{"data"});
+
+        // configure styling attributes
+
+
+        $attr->{"width"} = intval(floor(($width - (($points - 1) * 3)) / $points));
         $attr->{"fill"} = "#cc0000";
         $attr->{"stroke"} = "#00cc00";
-        $attr->{"stroke-width"} = 10;
-        $chart = ($chart . self::BuildElement("rect", $attr));
+        $attr->{"stroke-width"} = 1;
+
+        // determine maximum height
+
+
+        $max = $chartData->{"datasets"}[0]->{"data"}[0];
+        $i = 1;
+        while (($i < $points)) {
+            if (($chartData->{"datasets"}[0]->{"data"}[$i] > $max)) {
+                $max = $chartData->{"datasets"}[0]->{"data"}[$i];
+            }
+            $i = ($i + 1);
+        }
+
+        // create bars
+
+        $i = 0;
+        while (($i < $points)) {
+            $attr->{"x"} = (($attr->{"width"} * $i) + (3 * $i));
+            $attr->{"height"} = round(($height * ($chartData->{"datasets"}[0]->{"data"}[$i] / $max)), 0);
+            $attr->{"y"} = ($height - $attr->{"height"});
+            $chart = ($chart . self::rafBuildElement("rect", $attr));
+            $i = ($i + 1);
+        }
 
         $chart = ($chart . "</svg>");
 
         return $chart;
     }
 
-    // Build SVG element
-    public static function BuildElement($name, $attributes) {
+    // ***** rafBuildElement ********************
+
+    public static function rafBuildElement($name, $attributes) {
         $element = "";
         $i = 0;
 
