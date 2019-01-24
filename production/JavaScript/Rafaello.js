ad2fb547ebb1e4c13b3959875e9c5345699e6074
Alwyn Bester <alwyn.bester@futurelearn.co.za>
2019-01-24 13:32:49 +0200

    Partially implemented y-scale.

--- production/JavaScript/Rafaello.js
+++ production/JavaScript/Rafaello.js
@@ -9,6 +9,8 @@
         var points = 0;
         var i = 0;
         var max = 0;
+        var yScaleMargin = 0;
+        var maxScaleLen = 0;
 
         chart = '<svg ';
         chart = (((chart + " width=\"") + width) + "\"");
@@ -17,14 +19,10 @@
 
         points = Object.keys(chartData['datasets'][0]['data']).length;
 
-        // configure styling attributes
+        // determine scale margins
 
-        attr['width'] = parseInt(Math.floor((width - ((points - 1) * 5)) / points));
-        attr['fill'] = '#00cc00';
-        attr['fill-opacity'] = 0.75;
-        attr['stroke'] = '#004000';
-        attr['stroke-opacity'] = 0.75;
-        attr['stroke-width'] = 1;
+        maxScaleLen = 9;
+        yScaleMargin = (maxScaleLen * 9);
 
         // determine maximum height
 
@@ -37,6 +35,35 @@
             i = (i + 1);
         }
 
+        // y-axis
+
+        attr = JSON.parse('{}');
+        attr['x1'] = ((width - yScaleMargin) + 5);
+        attr['y1'] = 0;
+        attr['x2'] = ((width - yScaleMargin) + 5);
+        attr['y2'] = height;
+        attr['stroke'] = '#000000';
+        attr['stroke-width'] = 1;
+        chart = (chart + Rafaello.rafBuildElement('line', attr, ''));
+
+        // scale values
+
+        attr = JSON.parse('{}');
+        attr['x'] = ((width - yScaleMargin) + 10);
+        attr['y'] = 100;
+        attr['fill'] = '#000000';
+        chart = (chart + Rafaello.rafBuildElement('text', attr, 'Test12345'));
+
+        // configure styling attributes
+
+        attr = JSON.parse('{}');
+        attr['width'] = parseInt(Math.floor(((width - ((points - 1) * 5)) - yScaleMargin) / points));
+        attr['fill'] = '#00cc00';
+        attr['fill-opacity'] = 0.75;
+        attr['stroke'] = '#004000';
+        attr['stroke-opacity'] = 0.75;
+        attr['stroke-width'] = 1;
+
         // create bars
 
         i = 0;
@@ -44,7 +71,7 @@
             attr['x'] = ((attr['width'] * i) + (5 * i));
             attr['height'] = (Math.round((height * (chartData['datasets'][0]['data'][i] / max)) * Math.pow(10, 0)) / Math.pow(10, 0));
             attr['y'] = (height - attr['height']);
-            chart = (chart + Rafaello.rafBuildElement('rect', attr));
+            chart = (chart + Rafaello.rafBuildElement('rect', attr, ''));
             i = (i + 1);
         }
 
@@ -55,7 +82,7 @@
 
     // ***** rafBuildElement ********************
 
-    Rafaello.rafBuildElement = function (name, attributes) {
+    Rafaello.rafBuildElement = function (name, attributes, content) {
         var element = '';
         var i = 0;
 
@@ -65,7 +92,12 @@
             element = (((((element + " ") + Object.keys(attributes)[i]) + "=\"") + attributes[Object.keys(attributes)[i]]) + "\"");
             i = (i + 1);
         }
-        element = (element + " />");
+        if ((content != "")) {
+            element = (((((element + ">") + content) + "</") + name) + ">");
+        } else {
+            element = (element + " />");
+        }
+
 
         return element;
     }
--- production/PHP/Rafaello.php
+++ production/PHP/Rafaello.php
@@ -11,6 +11,8 @@ class Rafaello {
         $points = 0;
         $i = 0;
         $max = 0;
+        $yScaleMargin = 0;
+        $maxScaleLen = 0;
 
         $chart = "<svg ";
         $chart = ((($chart . " width=\"") . $width) . "\"");
@@ -19,14 +21,10 @@ class Rafaello {
 
         $points = count($chartData->{"datasets"}[0]->{"data"});
 
-        // configure styling attributes
+        // determine scale margins
 
-        $attr->{"width"} = intval(floor(($width - (($points - 1) * 5)) / $points));
-        $attr->{"fill"} = "#00cc00";
-        $attr->{"fill-opacity"} = 0.75;
-        $attr->{"stroke"} = "#004000";
-        $attr->{"stroke-opacity"} = 0.75;
-        $attr->{"stroke-width"} = 1;
+        $maxScaleLen = 9;
+        $yScaleMargin = ($maxScaleLen * 9);
 
         // determine maximum height
 
@@ -39,6 +37,35 @@ class Rafaello {
             $i = ($i + 1);
         }
 
+        // y-axis
+
+        $attr = json_decode("{}");
+        $attr->{"x1"} = (($width - $yScaleMargin) + 5);
+        $attr->{"y1"} = 0;
+        $attr->{"x2"} = (($width - $yScaleMargin) + 5);
+        $attr->{"y2"} = $height;
+        $attr->{"stroke"} = "#000000";
+        $attr->{"stroke-width"} = 1;
+        $chart = ($chart . self::rafBuildElement("line", $attr, ""));
+
+        // scale values
+
+        $attr = json_decode("{}");
+        $attr->{"x"} = (($width - $yScaleMargin) + 10);
+        $attr->{"y"} = 100;
+        $attr->{"fill"} = "#000000";
+        $chart = ($chart . self::rafBuildElement("text", $attr, "Test12345"));
+
+        // configure styling attributes
+
+        $attr = json_decode("{}");
+        $attr->{"width"} = intval(floor((($width - (($points - 1) * 5)) - $yScaleMargin) / $points));
+        $attr->{"fill"} = "#00cc00";
+        $attr->{"fill-opacity"} = 0.75;
+        $attr->{"stroke"} = "#004000";
+        $attr->{"stroke-opacity"} = 0.75;
+        $attr->{"stroke-width"} = 1;
+
         // create bars
 
         $i = 0;
@@ -46,7 +73,7 @@ class Rafaello {
             $attr->{"x"} = (($attr->{"width"} * $i) + (5 * $i));
             $attr->{"height"} = round(($height * ($chartData->{"datasets"}[0]->{"data"}[$i] / $max)), 0);
             $attr->{"y"} = ($height - $attr->{"height"});
-            $chart = ($chart . self::rafBuildElement("rect", $attr));
+            $chart = ($chart . self::rafBuildElement("rect", $attr, ""));
             $i = ($i + 1);
         }
 
@@ -57,7 +84,7 @@ class Rafaello {
 
     // ***** rafBuildElement ********************
 
-    public static function rafBuildElement($name, $attributes) {
+    public static function rafBuildElement($name, $attributes, $content) {
         $element = "";
         $i = 0;
 
@@ -67,7 +94,12 @@ class Rafaello {
             $element = ((((($element . " ") . array_keys((array)$attributes)[$i]) . "=\"") . $attributes->{array_keys((array)$attributes)[$i]}) . "\"");
             $i = ($i + 1);
         }
-        $element = ($element . " />");
+        if (($content != "")) {
+            $element = ((((($element . ">") . $content) . "</") . $name) . ">");
+        } else {
+            $element = ($element . " />");
+        }
+
 
         return $element;
     }
